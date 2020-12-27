778b6765b5dbd9507a3e06d0233ece052d0d6f02
scott <grtwloop@gmail.com>
2020-12-26 22:07:56 -0500

    Test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
- return res.send("Hello Heroku");
+ return res.send("Hello Heroku test");
 });
 
 // listen to port 7000 by default
